bdf223a39a496084dbbdaa76db9f81af9378635a
mocha <skykww0714@gmail.com>
2022-05-02 09:35:36 +0900

    test

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A = 0;
+	A = 100;
+
+	cout << A + 3 << endl;
+	cout << A - 3 << endl;
+	cout << A * 3 << endl;
+	cout << A / 3 << endl;
+	cout << A % 3 << endl;
+
+	float B = 0;
+	B = 2.1f;
+	
+	cout << B + 0.3f << endl;
+	cout << B - 0.3f << endl;
+	cout << B * 0.3f << endl;
+	cout << B / 0.3f << endl;
+	
+
+
+	return 0;
+}
