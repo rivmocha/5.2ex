70ecbc2fe4ac6dad72f257998a6732bd4a8d4b4c
mocha <skykww0714@gmail.com>
2022-05-02 15:14:34 +0900

    * ** *** **** *****

--- main.cpp
+++ main.cpp
@@ -3,20 +3,16 @@ using namespace std;
 
 int main()
 {
-	int Sum1 = 0;
-	int Sum2 = 0;
-	
-	for (int i = 1; i <= 100; i = i++)
-	{
-		if (i % 2 == 1)
-		Sum1 += i;
-		
-		else
-		Sum2 += i;
-		
-	}
+	char X = '*';
 
-	cout << "1부터 100까지 중 홀수의 합은" << Sum1 << endl;
-	cout << "1부터 100까지 중 짝수의 합은" << Sum2 << endl;
+	for (int j = 1; j <= 5; j++)
+	{
+		for (int i = 0; i < j; i++)
+		{
+			cout << X;
+		}
+
+		cout << '\n';
+	}
 	return 0;
 }
