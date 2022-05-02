e979c9162c46dc8c4e141491c4cfb5c92acb7d01
mocha <skykww0714@gmail.com>
2022-05-02 10:12:27 +0900

    add endl

--- main.cpp
+++ main.cpp
@@ -6,20 +6,23 @@ int main()
 	int A = 0;
 	A = 100;
 
+
 	cout << A + 3 << endl;
 	cout << A - 3 << endl;
 	cout << A * 3 << endl;
 	cout << A / 3 << endl;
 	cout << A % 3 << endl;
+	cout << endl;
+
 
 	float B = 0;
 	B = 2.1f;
-	
+
 	cout << B + 0.3f << endl;
 	cout << B - 0.3f << endl;
 	cout << B * 0.3f << endl;
 	cout << B / 0.3f << endl;
-	
+
 
 
 	return 0;
