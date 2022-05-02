66da38bdda1ef9c34d82f66888e9c545203154f2
mocha <skykww0714@gmail.com>
2022-05-02 14:33:42 +0900

    1~100 홀수 짝수의 합

--- main.cpp
+++ main.cpp
@@ -3,13 +3,20 @@ using namespace std;
 
 int main()
 {
-	int Sum = 0;
-	for (int i = 0; i <= 100; i = i++)
+	int Sum1 = 0;
+	int Sum2 = 0;
+	
+	for (int i = 1; i <= 100; i = i++)
 	{
-		Sum = Sum + i;
+		if (i % 2 == 1)
+		Sum1 += i;
+		
+		else
+		Sum2 += i;
+		
 	}
 
-	cout << "1부터 100까지의 합은" << Sum << endl;
-
+	cout << "1부터 100까지 중 홀수의 합은" << Sum1 << endl;
+	cout << "1부터 100까지 중 짝수의 합은" << Sum2 << endl;
 	return 0;
 }
